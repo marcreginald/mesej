d7fe01b1fb887cf65589417b51cfae16aeabd23e
marcreginald <marcpanaligan98@gmail.com>
2022-12-28 03:37:43 +0800

    updated

---
+++ components/Sidebar.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import styled from 'styled-components';
+import { Avatar } from '@material-ui/core';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+
+function Sidebar() {
+  return (
+
+<Container>
+    <Header>
+        <UserAvatar/>
+
+        <IconsContainer>
+            <ChatIcon/>
+            <MoreVertIcon/>
+        </IconsContainer>
+    </Header>
+</Container>
+  )
+}
+
+export default Sidebar;
+
+const Container = styled.div``;
+
+const Header = styled.div``;
+
+const UserAvatar = styled(Avatar)``;
+
+const IconsContainer = styled.div``;
